5146bcfc1b61f75027812d5cb541f2a41711bc13
fishstack1142 <fishstack1142@gmail.com>
2019-08-15 23:12:34 +0700

    add product page

--- src/component/product/ProductItem.js
+++ src/component/product/ProductItem.js
@@ -36,9 +36,26 @@ class ProductItem extends React.Component {
                 </div>
                 <h5 className="mt-2 text-success">{productName}</h5>
                 <p className="title text-right" >{unitPrice} THB</p>
+
+                { this.props.onAddOrder &&
                 <button className="btn btn-block btn-secondary title" value={unitPrice} onClick={() => this.props.onAddOrder(this.props.product)}>
                     buy 
                 </button>
+                }
+
+                { (this.props.onDelProduct || this.props.onEditProduct) &&
+                <button className="btn btn-info col-6 title">
+                    edit 
+                </button>
+                }
+
+
+                { (this.props.onDelProduct || this.props.onEditProduct) &&
+                <button className="btn btn-danger col-5 float-right title" value={unitPrice} onClick={() => this.props.onDelProduct(this.props.product)}>
+                    remove 
+                </button>
+                }
+
                 <hr/>
             </div>
         );

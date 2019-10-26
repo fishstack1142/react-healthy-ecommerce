6aa7a7c71bca70c36d8e6e5d2e40938d81fe056a
fishstack1142 <fishstack1142@gmail.com>
2019-10-26 22:51:41 +0700

    Use redux in Product component

--- src/component/containers/product/Product.js
+++ src/component/containers/product/Product.js
@@ -6,20 +6,36 @@ import { withRouter } from "react-router-dom";
 
 import axios from "axios";
 
+import { connect } from "react-redux";
+import { productsFetch, productDelete } from "../../../redux/actions";
+
+
 class Product extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { products : null };
+        // this.state = { products : null };
         this.delProduct = this.delProduct.bind(this);
         this.editProduct = this.editProduct.bind(this);
 
     }
 
     componentDidMount() {
-        axios.get("http://localhost:3001/products").then(res => {
-            this.setState({ products : res.data });
-        });
+        // axios.get("http://localhost:3001/products").then(res => {
+        //     this.setState({ products : res.data });
+        // });
+        this.props.productsFetch();
+        console.log('from props page');
+        console.log(this.props);
+    }
+
+    componentDidUpdate() {
+        // axios.get("http://localhost:3001/products").then(res => {
+        //     this.setState({ products : res.data });
+        // });
+        // this.props.productsFetch();
+        console.log('from props update');
+        console.log(this.props);
     }
 
     editProduct(product) {
@@ -27,12 +43,13 @@ class Product extends Component {
     }
 
     delProduct(product) {
-        axios.delete("http://localhost:3001/products/" + product.id).then(res => {
-            axios.get("http://localhost:3001/products/").then(
-                res => {
-                    this.setState({ products : res.data });
-                });
-        });
+        // axios.delete("http://localhost:3001/products/" + product.id).then(res => {
+        //     axios.get("http://localhost:3001/products/").then( 
+        //         res => {
+        //             this.setState({ products : res.data });
+        //         });
+        // });
+        this.props.productDelete(product.id);
     }
 
 
@@ -51,7 +68,8 @@ class Product extends Component {
                                 onClick={() => this.props.history.push('products/add') } >add</button>
                             </div>
                         </div>
-                        <ProductList products={this.state.products} 
+
+                        <ProductList products={this.props.products} 
                         onDelProduct={this.delProduct}
                         onEditProduct={this.editProduct} /> 
                     </div>
@@ -63,4 +81,10 @@ class Product extends Component {
     }
 }
 
-export default withRouter(Product);
+
+function mapStateToProps({ product }){
+    return { products : product };
+}
+
+
+export default withRouter(connect(mapStateToProps, { productsFetch, productDelete })(Product));
